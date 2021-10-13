1e5552d0eedd80836919fb0c1c613b330d202fa7
gpaetzold <76201189+gpaetzold@users.noreply.github.com>
2021-10-13 16:43:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,8 @@
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
     if (pins.digitalReadPin(DigitalPin.P1) == 1) {
-        basic.showLeds(`
-            # # # # #
-            . # . # .
-            . . # . .
-            . # . # .
-            # # # # #
-            `)
+        basic.showIcon(IconNames.Yes)
     } else {
-    	
+        basic.showIcon(IconNames.No)
     }
 })
